ff69d8581421629d04d4d7652e543541c4629b7b
Sergio De Luca <sdlbsso@gmail.com>
2022-08-21 16:58:43 -0300

    ver pais ok haciendo por region

--- src/app/paises/pages/por-region/por-region.component.ts
+++ src/app/paises/pages/por-region/por-region.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent{
 
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+
+  regionActiva: string="";
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion(region: string){
+    this.regionActiva = region;
+    console.log(region);
+    //traer desde el servicio
   }
 
 }
